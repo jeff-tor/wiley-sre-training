0f60647abfb9f15816fbfc140a97721b206e3664
jeff-tor <jjiaaffuu@hotmail.com>
2022-08-06 14:30:05 +0800

    Commit for SQL Quiz Exercise

--- TorJiaFu-HotelQueries.sql
+++ TorJiaFu-HotelQueries.sql
@@ -38,14 +38,38 @@ WHERE a.Name = 'Jeremiah Pendergrass'
 
 --Query to return list of rooms, reservation ID, and per-room cost for each reservation. The results should include all rooms, whether or not there is a reservation associated with the room.
 
+SELECT a.GuestReservationID,
+bRoomNumber,
+b.BasePrice,
+FROM Reservations a.
+LEFT JOIN Rooms b
+ON a.RoomID = b.RoomID
+
 --Query to return all the rooms accomodating at least three guests and that are reserved on any date in April 2023
 
+SELECT a.RoomNumber,
+
+FROM Rooms a
+LEFT JOIN Reservations b
+
+WHERE a.StartDate >= "2022-04-01"
+AND a.StartDate <= "2022-05-01"
+HAVING count(b.GuestReservationID) >= 3
+
 --Query to return list of guest names and number of reservations per guest, sorted starting with the guest with the most reservation, then by the guests name.
+
+SELECT count(b.GuestReservationID),
+a.Name,
+FROM Guests a
+LEFT JOIN Reservations b
+a.RoomID = b.RoomID
+ORDER BY count(b.GuestReservationID) DESC
 
 --Query to display the name, address, phone number baesd on their phone number
 
 SELECT Name,
 Address,
 PhoneNumber
+FROM Guests
+WHERE PhoneNumber = '89416741'
 
-WHERE PhoneNumber = 'xxxxxxxx'
