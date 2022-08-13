87d1389f57f4a53988c5d63605ffc6288f85afd4
jeff-tor <jjiaaffuu@hotmail.com>
2022-08-13 15:08:23 +0800

    Commit for Python Exercise 2

--- Python/Activities-Inheritance.py
+++ Python/Activities-Inheritance.py
@@ -1,9 +1,9 @@
 #activity 0
 
 class movies:
-    name = None
-    genre = None
-    def __init__(self, gname, ggenre):
+name = None
+genre = None
+    def __init__(self, gname, genre):
         self.name = name
         self.genre = genre
     def display(self):
@@ -13,8 +13,8 @@ class movies:
 class videos(movies):
 
 
-    platform = None
-    players = None
+platform = None
+players = None
     def __init__(self, name, genre, platform):
         movies.__init__(self, name, genre)
         self.platform = platform
@@ -22,3 +22,41 @@ class videos(movies):
     def display(self):
         games.display(self)
         print("This video is on the platform", self.platform)
+
+
+class tiktok_videos(movies):
+
+platform = None
+viewers = None
+availability = None
+    def __init__(self, name, genre, availability)
+        self.name = name
+        self.genre = genre
+        self.availability = availability
+
+    def display(self):
+        movies.display(self)
+        print("This video is restricted for friends only")
+        if self.availability:
+            print("This video is private")
+        else:
+            print("This video is for public viewing")
+
+#activity 4
+
+TheBigMan = movies("TheBigMan", "Horror")
+TheSmallMan = movies("TheSmallMan", "Comedy")
+TheMediumMan = movies("TheMediumMan", "Sitcom")
+HowToDance = videos("HowToDance", "Educational","Youtube")
+HowToMSexcel = videos("HowToMSexcel","Educationa","Youtube")
+PythonBasics = videos("PythonBasics","Educational","Coursera")
+janjan_01 = tiktok_videos("HappyHour","Casual","Private")
+janjan_02 = tiktok_videos("AngryHour","Horror","Public")
+TheBigMan.display()
+TheSmallMan.display()
+TheMediumMan.display()
+HowToDance.display()
+HowToMSexcel.display()
+PythonBasics.display()
+janjan_02.display()
+janjan_01.display()
