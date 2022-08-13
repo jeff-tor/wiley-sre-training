1ea6a2caec819b3fb7b77e870d8259f0b0aff04b
jeff-tor <jjiaaffuu@hotmail.com>
2022-08-13 15:29:25 +0800

    Commit for Python Exercise 2:

---
+++ Python/SelfAssessment-Inheritance.py
@@ -0,0 +1,32 @@
+class fileItem:
+    name = None
+    permissions = None
+    os_owner = None
+    size = None
+    creationDate = None
+
+    def __init__(self, name, pem, own, size, crtDate):
+        self.name = name
+        self.pem = pem
+        self.own = own
+        self.size = size
+        self.creationDate = crtDate
+
+    def display(self):
+        print(self.name, self.pem, self.own, self.size, self.creationDate)
+
+
+
+class CsvFile:
+    def __init__(self, filename, filesize):
+        self.filename = filename
+        self.filesize = filesize
+
+        
+
+class JpgFile
+    pass
+
+
+class Mp3File
+    pass
